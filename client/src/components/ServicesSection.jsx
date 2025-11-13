5f5ce12bd7b9e7710dffb40786bae77ef60a8d30
kombae1 <kombae@matc.edu>
2025-11-13 11:45:55 -0600

    added menu.jsx file so everyone can access the api endpoints

--- client/src/components/ServicesSection.jsx
+++ client/src/components/ServicesSection.jsx
@@ -1,9 +1,28 @@
+import { useState, useEffect } from "react";
+import { getMenu } from "../Menu";
+
 export default function ServicesSection() {
+  const [menu, setMenu] = useState([]);
+  
+  useEffect(() => {
+    getMenu()
+    .then((items) => setMenu(items)).
+    catch((err) => console.error(err));
+  }, []);
+  
   return (
     <section id="services" className="panel">
       <div className="container">
         <h2>Services</h2>
+        {menu.map((item) => (
+          <div key={item.id}>
+            <h3>{item.name}</h3>
+            <p>Price: ${item.price}</p>
+            <p>{item.description}</p>
+          </div>
+        ))}
         <p>MENU</p>
+        
       </div>
     </section>
   );
