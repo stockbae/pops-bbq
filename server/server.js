d170a4c5db44f76d3b8c7707810e33621c8496e5
Ethan Stockbauer <stockbae@matc.edu>
2025-11-27 01:58:28 -0600

    password protect employee dashboard

--- server/server.js
+++ server/server.js
@@ -4,6 +4,7 @@ import menuRoutes from "./routes/menuRoutes.js";
 import meatRoutes from "./routes/meatRoutes.js";
 import sideRoutes from "./routes/sideRoutes.js";
 import orderRoutes from "./routes/orderRoutes.js";
+import authRoutes from "./routes/authRoutes.js";
 
 
 const app = express();
@@ -15,5 +16,6 @@ app.use("/api/menu", menuRoutes);
 app.use("/api/meats", meatRoutes);
 app.use("/api/sides", sideRoutes);
 app.use("/api/orders", orderRoutes);
+app.use("/api/auth", authRoutes);
 
 app.listen(5000, () => console.log("Server running on port 5000"));
