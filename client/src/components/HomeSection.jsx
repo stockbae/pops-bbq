f7a171509ebdf311b81540c5526dc1a436dd32dd
[Zack Pileggi} <pileggiz@gmatc.matc.edu>
2025-12-01 20:07:20 -0600

    added order button in home section

--- client/src/components/HomeSection.jsx
+++ client/src/components/HomeSection.jsx
@@ -1,10 +1,23 @@
 import '../App.css';
 
 export default function HomeSection() {
+
+  const scrollToServices = (e) => {
+    e.preventDefault();
+    document.querySelector('.nav-link[href="#services"]').click();
+  };
   return (
     <section id="home" className="panel">
-      <div className="container">
+      <div className="container home-header-row">
         <h1>Home</h1>
+        
+
+        <button className="home-button" onClick={scrollToServices}>
+          Click to Order
+        </button>
+      </div>
+
+      <div className="container">
         <p>Welcome to Pop's BBQ!</p>
       </div>
     </section>
--- client/src/pages/Home.jsx
+++ client/src/pages/Home.jsx
@@ -16,7 +16,7 @@ export default function Home() {
       <main>
         <HomeSection />
         <AboutSection />
-        {/* <ServicesSection /> */}
+        <ServicesSection /> 
         <BuildOrder />
         <PortfolioSection />
         <ContactSection />
